7e9e3850fb6c64bb98a59edea140e6fcc65afc53
jbocane6 <jbocane6@gmail.com>
2021-06-17 19:52:07 -0700

    file 101 v.3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - first function to execute when run program
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-std::cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
