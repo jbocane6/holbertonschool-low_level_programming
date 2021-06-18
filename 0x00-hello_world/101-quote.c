6cd1336daa6352d371e91209a53c03287fd443cf
jbocane6 <jbocane6@gmail.com>
2021-06-17 19:42:24 -0700

    file 101 v.1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <iostream>
+/**
+ * main - first function to execute when run program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+std::cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+return (1);
+}
