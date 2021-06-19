c25142754cfc3824728c9aa9c85d30713125ae2e
jbocane6 <jbocane6@gmail.com>
2021-06-18 20:25:14 -0700

    file 9 v.1

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - first function to execute when run program
+ * prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+for (n = '0' ; n <= '9' ; n++)
+{
+putchar(n);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
