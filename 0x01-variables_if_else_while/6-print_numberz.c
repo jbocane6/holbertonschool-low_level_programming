05e14f78ccd9ac24dae00974a9e9fa6585b5e1f9
jbocane6 <jbocane6@gmail.com>
2021-06-18 20:05:10 -0700

    file 6 v.1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - first function to execute when run program
+ * print all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 0 ; n < 10 ; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
