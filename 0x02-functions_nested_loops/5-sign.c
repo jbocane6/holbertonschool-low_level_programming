a96085130f1fa8ef1efd42613b8ee10346c323f7
jbocane6 <jbocane6@gmail.com>
2021-06-22 14:22:53 -0700

    file 5 v.1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_sign - first function
+ * @n: int type number
+ * Return: 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf("+")
+return (1);
+}
+else if (n < 0)
+{
+printf("-")
+return (-1);
+}
+else
+{
+printf("0")
+return (0);
+}
+}
