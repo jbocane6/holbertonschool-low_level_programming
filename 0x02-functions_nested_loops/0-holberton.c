13395e76a47996c68680161707a43919a375e8a5
jbocane6 <jbocane6@gmail.com>
2021-06-22 13:27:51 -0700

    file 0 v.2

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,11 @@
-#include <stdio.h>
+#include <holberton.h>
 /**
  * main - first function to execute when run program
- * prints "Holberton"
+ * print alphabet lower case
  * Return: 0
  */
 int main(void)
 {
-char holberton[] = "Holberton";
-int i = 0;
-
-while (holberton[i] != '\0')
-{
-putchar(holberton[i]);
-i++;
-}
-putchar('\n');
-return (0);
+  print_alphabet();
+  return (0);
 }
