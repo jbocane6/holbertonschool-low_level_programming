a4bdcb9edfd95b115c8ac707467aa5250994eee2
jbocane6 <jbocane6@gmail.com>
2021-06-22 14:23:59 -0700

    file 5 v.2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+")
+printf("+");
 return (1);
 }
 else if (n < 0)
 {
-printf("-")
+printf("-");
 return (-1);
 }
 else
 {
-printf("0")
+printf("0");
 return (0);
 }
 }
