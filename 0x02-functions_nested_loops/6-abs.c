b2be01fa39cd0cef356ae3d165e315e65d6d1bd7
jbocane6 <jbocane6@gmail.com>
2021-06-22 17:14:10 -0700

    file 6 v.1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _abs - first function
+ * @n: int type number
+ * Return: 0
+ */
+int _abs(int n)
+{
+int a;
+
+a = abs(n);
+_putchar(a);
+_putchar('\n');
+}
