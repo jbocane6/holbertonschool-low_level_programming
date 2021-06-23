21dde2a66fb5527708b4f898000c796446b35289
jbocane6 <jbocane6@gmail.com>
2021-06-22 20:23:33 -0700

    file 10 v.1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - first function
+ * @a: int value number
+ * @b: int value number
+ * Return: 0
+ */
+int add(int a, int b)
+{
+int res;
+res = a + b;
+return (res);
+}
