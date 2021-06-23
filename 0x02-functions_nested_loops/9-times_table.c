0489afc58badcd9485c822420189cf47cc08b6f4
jbocane6 <jbocane6@gmail.com>
2021-06-22 19:33:02 -0700

    file 9 v.1

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * times_table - first function
+ * Return: 0
+ */
+void times_table(void)
+{
+int mult;
+int base;
+int prod;
+
+for (base = 0; base <= 9; base++)
+{
+for (mult = 0; mult <= 9; mult++)
+{
+prod = base * mult;
+_putchar(prod + '0');
+if (mult < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+else
+_putchar('\n');
+}
+}
