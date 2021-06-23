777c273ba1d85f84e61970d6a8b832f6c88fe60a
jbocane6 <jbocane6@gmail.com>
2021-06-22 19:44:59 -0700

    file 9 v.2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,10 +14,15 @@ for (base = 0; base <= 9; base++)
 for (mult = 0; mult <= 9; mult++)
 {
 prod = base * mult;
+if (prod <= 9)
 _putchar(prod + '0');
+else
+{
+_putchar(prod / 10 + '0');
+_putchar(prod % 10 + '0');
+}
 if (mult < 9)
 {
-_putchar(',');
 _putchar(' ');
 }
 else
