614bb818ac75ab1b83df8deb0bf2a2431589fb9e
jbocane6 <jbocane6@gmail.com>
2021-06-22 19:22:05 -0700

    file 8 v.3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,5 +5,25 @@
  */
 void jack_bauer(void)
 {
-jack_bauer();
+int min;
+int hor;
+int bur;
+int buja;
+
+for (hor = 0; hor <= 23; hor++)
+{
+for (min = 0; min <= 59; min++)
+{
+bur = hor / 10;
+buja = hor % 10;
+putchar(bur + '0');
+putchar(buja + '0');
+putchar(':');
+bur = min / 10;
+buja = min % 10;
+putchar(bur + '0');
+putchar(buja + '0');
+putchar('\n');
+}
+}
 }
