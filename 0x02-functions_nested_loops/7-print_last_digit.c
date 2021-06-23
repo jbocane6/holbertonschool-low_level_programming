1495f28035a57d928a88776455fa05cfc3f42123
jbocane6 <jbocane6@gmail.com>
2021-06-22 17:26:22 -0700

    ile 7 v.1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_last_digit - first function
+ * @n: int type number
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+int a;
+
+a = n % 10;
+return (a);
+}
