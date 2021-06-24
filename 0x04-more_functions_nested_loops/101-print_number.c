ec19dcefea362ac7e40017be1969493f5fece4a1
jbocane6 <jbocane6@gmail.com>
2021-06-24 15:16:05 -0700

    file 101 v.1

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * print_number -  function that prints an integer
+ * @n: int value number
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	if (n == 0)
+		_putchar(n + '0');
+	else
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n = n * -1;
+		}
+		int len = ceil(log10(n));
+		int cont, cont2;
+		int i = 1;
+
+		for (cont2 = 2; cont2 <= len; cont2++)
+		{
+			i = i * 10;
+		}
+		for (cont2 = 2; cont2 <= len; cont2++)
+		{
+			_putchar((n / i) + '0');
+			n = n % i;
+			i = i / 10;
+		}
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
