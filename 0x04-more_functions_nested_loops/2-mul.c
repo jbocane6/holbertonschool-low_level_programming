1edfbfce77d5398b9f1b69b39b8add32abb591fc
jbocane6 <jbocane6@gmail.com>
2021-06-24 10:29:06 -0700

    file 2 v.2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 #include "holberton.h"
 
 /**
- * mul -  function that multiplies two integers
+ * mul - function that multiplies two integers
  * @a: int value number
  * @b: int value number
  * Return: 0
@@ -10,6 +10,6 @@
 
 int mul(int a, int b)
 {
-	printf("%d", a * b);
+	printf("%d", (a * b));
 	return (0);
 }
