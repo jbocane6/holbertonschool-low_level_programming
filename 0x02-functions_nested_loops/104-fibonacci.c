4f42d08baccdc14f3d600d5a4dc5b61b408257fc
jbocane6 <jbocane6@gmail.com>
2021-06-23 21:41:33 -0700

    file 104 v.14

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,22 +8,30 @@
 
 int main(void)
 {
-	unsigned long int n1 = 1;
-	unsigned long int n2 = 2;
-	unsigned long int burbuja;
-	unsigned long int con;
+	unsigned long int n1 = 1, n2 = 2, burbuja, con;
+	unsigned long int temp1, temp2, temp3, izq1, izq2;
 
 	printf("%lu", n1);
 	printf(", ");
 	printf("%lu", n2);
 	printf(", ");
-	for (con = 3; con <= 98; con++)
+	for (con = 3; con <= 91; con++)
 	{
 		burbuja = n2;
 		n2 = n2 + n1;
 		n1 = burbuja;
 		printf("%lu", n2);
-		if (con != 97)
+		printf(", ");
+	}
+	for (con = 92; con <= 98; con++)
+	{
+		temp1 = (n1 + n2) / 10000000000;
+		temp2 = (n1 + n2) % 10000000000;
+		temp3 = izq1 + izq2 + temp1;
+		izq1 = izq2, izq2 = temp3;
+		n1 = n2, n2 = temp2;
+		printf("%lu%010lu", izq2, n2);
+		if (con != 98)
 		{
 			printf(", ");
 		}
@@ -31,4 +39,3 @@ int main(void)
 	printf("\n");
 	return (0);
 }
-
