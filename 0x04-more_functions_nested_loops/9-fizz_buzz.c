6c042ec1a28bfd5f20009b9ab159fd552caa8462
jbocane6 <jbocane6@gmail.com>
2021-06-24 21:18:33 -0700

    corrección file 9 v.5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include <stdio.h>
 
 /**
  * main - main block, solve fizz buzz from numbers 1 to 100
@@ -6,23 +6,25 @@
  * Multiples of both 3 and 5 should print FizzBuzz.
  * Return: 0
  */
-
 int main(void)
 {
-	int con;
+	int i;
 
-	for (con = 1; con <= 100; con++)
+	i = 1;
+	while (i <= 100)
 	{
-		if (con % 3 == 0 && con % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		else if (con % 5 == 0)
-			printf("Buzz");
-		else if (con % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
-			printf("%i ", con);
-		if (con != 100)
+			printf("%d", i);
+
+		if (i != 100)
 			printf(" ");
+		i++;
 	}
 	printf("\n");
 
