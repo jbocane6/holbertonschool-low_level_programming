3ed33e9485e5ea9357649866349f68d35a50ec70
jbocane6 <jbocane6@gmail.com>
2021-06-24 21:29:22 -0700

    corrección file 100 v.3

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,26 +2,26 @@
 #include <math.h>
 
 /**
- * main - main function
+ * main - main block
  * Description: Find and print the largest prime factor of the number
  * 612852475143
  * Return: 0
  */
 int main(void)
 {
-	float limit = 612852475143;
-	int raiz = (int)floor(sqrt(limit));
-	int con, temp, fin;
+    float limit = 612852475143;
+    int raiz = (int)floor(sqrt(limit));
+    int con, temp, fin;
 
-	for (con = 2; con <= raiz - 1; con++)
+    for (con = 2; con <= raiz - 1; con++)
+    {
+	temp = raiz % con;
+	if (temp == 0)
 	{
-		temp = raiz % con;
-		if (temp == 0)
-		{
-			fin = con;
-		}
-		printf("%i \n", fin);
+	    fin = con;
 	}
+	printf("%i \n", fin);
+    }
 
-	return (0);
+    return (0);
 }
