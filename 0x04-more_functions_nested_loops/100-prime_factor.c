3f441d5264a85a809333e61cbc3891d2f1e8464c
jbocane6 <jbocane6@gmail.com>
2021-06-24 21:41:03 -0700

    Corrección cambios

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,21 +10,19 @@
 
 int main(void)
 {
-    float limit = 612852475143;
-    int raiz = (int)floor(sqrt(limit));
-    int con, temp, fin;
+	float limit = 612852475143;
+	int raiz = (int)floor(sqrt(limit));
+	int con, temp, fin;
 
-    for (con = 2; con <= raiz - 1; con++)
-    {
-	raiz % con;
-	if (temp == 0)
+	for (con = 2; con <= raiz - 1; con++)
 	{
-	    fin = con;
+		temp = raiz % con;
+		if (temp == 0)
+		{
+			fin = con;
+		}
+		printf("%i \n", fin);
 	}
 
-	printf("%i \n", fin);
-
-    }
-
-    return (0);
+	return (0);
 }
