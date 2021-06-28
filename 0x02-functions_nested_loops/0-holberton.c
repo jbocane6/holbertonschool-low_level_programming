8cae6872bd3cb4a93a46975f321b26e8b7bd4510
jbocane6 <jbocane6@gmail.com>
2021-06-28 15:25:15 -0700

    Por alguna razón tocó realizar nuevamente este archivo

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <holberton.h>
+#include <stdio.h>
 /**
  * main - first function to execute when run program
  * prints "Holberton"
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-  char holberton[] = "Holberton";
-  int i = 0;
+	char holberton[] = "Holberton";
+	int i = 0;
 
-  while (holberton[i] != '\0')
-    {
-      putchar(holberton[i]);
-      i++;
-    }
-  putchar('\n');
-  return (0);
+	while (holberton[i] != '\0')
+	{
+		putchar(holberton[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
