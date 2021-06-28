a304266a082f9fe7666d79adabd2dc7a5adfbdd1
jbocane6 <jbocane6@gmail.com>
2021-06-28 09:51:33 -0700

    file 6 v.2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,18 +13,18 @@ void puts_half(char *str)
 {
 	int c, d;
 
-	for (c = 0; s[c] != '\0'; c++)
+	for (c = 0; str[c] != '\0'; c++)
 		;
 
 	if (c % 2 == 0)
 		d = c / 2;
 	else
-		d = (c + 1) / 2
+		d = (c + 1) / 2;
 
-			while (d < c)
-			{
-				_putchar(str[d]);
-				d++;
-			}
+	while (d < c)
+	{
+		_putchar(str[d]);
+		d++;
+	}
 	_putchar('\n');
 }
