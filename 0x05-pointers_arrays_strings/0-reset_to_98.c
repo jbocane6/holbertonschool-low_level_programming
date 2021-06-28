53013269506839ed52c70288f6781a2f134684a4
jbocane6 <jbocane6@gmail.com>
2021-06-28 07:39:13 -0700

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - main function
+ *
+ * Description: takes a pointer to an int as parameter and
+ * updates the value it points to to 98
+ *
+ * @n: int type pointer
+ */
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+## holbertonschool-low_level_programming
+
+### 0x05. Repository with scripts of evidences:
+
+- 0x05. C - Pointers, arrays and strings
