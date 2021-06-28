d4d2b79ddea40a4c3fd3cb5914f14b698a1a16f7
jbocane6 <jbocane6@gmail.com>
2021-06-28 09:30:40 -0700

    file 6 v.1

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * puts2 - main function
+ *
+ * Description: prints every other character of a string,
+ * starting with the first character
+ *
+ * @str: char type pointer
+ *
+ */
+
+void puts2(char *str)
+{
+	int c, d;
+
+	for (c = 0; *str[c] != '\0'; c++)
+		;
+
+	c--;
+	d = 0;
+
+	while (d < c)
+	{
+		_putchar(*str[d]);
+		d + = 2;
+	}
+	_putchar('\n');
+}
