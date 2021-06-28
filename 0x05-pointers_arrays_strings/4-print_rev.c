43e12897a8bdab8be433b2e9debee7a637499d61
jbocane6 <jbocane6@gmail.com>
2021-06-28 08:43:55 -0700

    file 4 v.1

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_rev - main function
+ *
+ * Description: prints a string, in reverse, followed by a new line
+ *
+ * @s: char type pointer
+ *
+ */
+
+void print_rev(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+		;
+	while (c >= 0)
+	{
+		_putchar(s[c]);
+		c--;
+	}
+	_putchar('\n');
+}
