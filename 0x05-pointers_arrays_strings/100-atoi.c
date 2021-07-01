9748b2f6626fb082647a32f6d0b8c3fb277b6743
jbocane6 <jbocane6@gmail.com>
2021-06-30 19:45:14 -0700

    file 100 v.1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: String value input
+ *
+ * Return: An integer found in the string.
+ */
+int _atoi(char *s)
+{
+	int signo = 1;
+	unsigned int numero = 0;
+
+	do {
+		if (*s == '-')
+			signo = signo * -1;
+
+		else if (*s > 47 && *s < 58)
+			numero = (numero * 10) + (*s - '0');
+		else if (numero > 0)
+			break;
+
+	} while (*s++);
+
+	return (numero * signo);
+}
