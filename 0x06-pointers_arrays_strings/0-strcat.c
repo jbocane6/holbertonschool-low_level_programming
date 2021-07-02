2616da575b1ea15cfe1bace126fc42586a39117a
jbocane6 <jbocane6@gmail.com>
2021-07-02 12:39:16 -0700

    file 0 v.2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,30 +12,20 @@
 char *_strcat(char *dest, char *src)
 {
 	int cont_src = 0, cont_dest = 0;
-	int i = 0, j = 0;
-	int cont = 0;
-
-	while (src[cont_src] != '\0')
-	{
-		cont_src++;
-	}
 
 	while (dest[cont_dest] != '\0')
 	{
 		cont_dest++;
 	}
 
-	i = cont_dest + cont_src + 1;
-
-	dest = (char *) realloc(dest, i);
-	dest[cont_dest] = '';
-	dest[i - 1] = '\0';
-
-	for (j = cont_dest; j < i - 1; j++)
+	while (src[cont_src] != '\0')
 	{
-		dest[j] = src[cont];
-		cont++;
+		dest[cont_dest] = src[cont_src];
+		cont_src++;
+		cont_dest++;
 	}
+
+	dest[cont_dest] = '\0';
 
 	return (dest);
 }
