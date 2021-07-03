d7afea67b2ebf117bc4c073fb79e53bd147d4f2f
jbocane6 <jbocane6@gmail.com>
2021-07-02 18:05:58 -0700

    file 100 v.1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rot13 - Main function
+ * Description: function that encodes a string using rot13.
+ * @str: Pointer to a char value
+ * Return: String.
+ */
+
+char *rot13(char *str)
+{
+	int cont, cont2;
+	char cadena1[] = {"ABCDEFGHIJKLMabcdefghijklm"};
+	char cadena2[] = {"NOPQRSTUVWXYZnopqrstuvwxyz"};
+
+	if (str == 0)
+		return (0);
+
+	for (cont = 0; str[cont] != '\0'; cont++)
+	{
+		for (cont2 = 0; cadena1[cont2]; cont2++)
+		{
+			if (cadena1[cont2] == str[cont])
+			{
+				str[cont] = cadena2[cont2];
+			}
+		}
+	}
+	return (str);
+}
