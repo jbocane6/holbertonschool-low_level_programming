0e4afa7ff3bc5eb68ee49a2ff6553ff04b1b3a6b
jbocane6 <jbocane6@gmail.com>
2021-07-02 21:12:51 -0700

    file 104 v.1

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_buffer - Main function
+ * Description: function that prints a buffer.
+ * @b: char pointer value
+ * @size: Int value number
+ */
+
+void print_buffer(char *b, int size)
+{
+}
