0472647c97d587fa0d628aacece389d0f666b0b5
jbocane6 <jbocane6@gmail.com>
2021-07-02 18:12:27 -0700

    file 100 v.3

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stddef.h>
 
 /**
  * rot13 - Main function
@@ -13,6 +14,9 @@ char *rot13(char *str)
 	char cadena1[] = {"ABCDEFGHIJKLMabcdefghijklm"};
 	char cadena2[] = {"NOPQRSTUVWXYZnopqrstuvwxyz"};
 
+	if (str == NULL)
+		return (NULL);
+
 	for (cont = 0; str[cont] != '\0'; cont++)
 	{
 		for (cont2 = 0; cadena1[cont2]; cont2++)
@@ -23,5 +27,6 @@ char *rot13(char *str)
 			}
 		}
 	}
+
 	return (str);
 }
