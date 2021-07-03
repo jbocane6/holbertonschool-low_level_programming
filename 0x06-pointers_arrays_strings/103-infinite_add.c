f4b2370dbd7e708308adcc8d8bb1a76ce8ec6cb5
jbocane6 <jbocane6@gmail.com>
2021-07-02 21:11:18 -0700

    file 103 v.1

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * infinite_add - Main function
+ * Description: function that adds two numbers.
+ * @n1: char pointer value
+ * @n2: char pointer value
+ * @r: char pointer value
+ * @size_r: Int value number
+ * Return: char
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (n1);
+}
