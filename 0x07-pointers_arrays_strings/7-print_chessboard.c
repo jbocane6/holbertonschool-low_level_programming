073c2c89daca768491bb0f52cffa88256774a17b
jbocane6 <jbocane6@gmail.com>
2021-07-07 11:46:33 -0700

    file 7 v.1

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Main function
+ * Description: function prints the chessboard.
+ * @a: char pointer value
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (j == 7)
+				printf("\n");
+			else
+				printf("%c", a[i][j]);
+		}
+	}
+}
