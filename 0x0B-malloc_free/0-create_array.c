ae5fd7760901efebee41b7e41fdff5001bcdd2a4
jbocane6 <jbocane6@gmail.com>
2021-07-13 14:25:21 -0700

    file 0 v.1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char.
+ * @size: size of array
+ * @c: specific char
+ * Return: pointer to the array, or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *cadena;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	cadena = malloc(size * sizeof(c));
+	if (cadena == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		cadena[i] = c;
+
+	return (cadena);
+}
