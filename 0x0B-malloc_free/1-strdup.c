3123ad9647f545e0e7973e5560c01ebf5ff977d9
jbocane6 <jbocane6@gmail.com>
2021-07-13 14:34:36 -0700

    file 1 v.1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer which contains a copy of the string.
+ * @str: inter to a string
+ * Return: pointer to the new array, or NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+	char *cadena;
+	int size = 0, i;
+
+	for (size; str[size] != '\0'; size++)
+		;
+	str--;
+
+	if (str == 0)
+		return (NULL);
+
+	cadena = malloc(size * sizeof(cadena));
+
+	for (i = 0; i < size; i++)
+		cadena[i] = str[i];
+
+	return (&cadena);
+}
