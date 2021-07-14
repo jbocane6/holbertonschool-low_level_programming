bd48a4fed78d863fadfc85fe963eea7edd7f3aa3
jbocane6 <jbocane6@gmail.com>
2021-07-14 13:04:01 -0700

    file 4  v.3

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,14 +8,12 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i, j;
+	int i;
 
 	if (grid == NULL || height <= 0)
-		return (NULL);
+		return;
 
 	for (i = 0; i < height; i++)
 		free(grid[i]);
 	free(grid);
-
-	return (grid);
 }
