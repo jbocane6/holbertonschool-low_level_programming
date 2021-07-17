667d7a8512851173d5a9c3e4ca9e2f6c17e442e1
root <root@DESKTOP-DVQ378U.localdomain>
2021-07-17 15:01:09 -0500

    file 4 v.4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,7 +30,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	str = malloc(sizeof(ptr) * new_size);
+	str = malloc(new_size);
 	if (str == NULL)
 		return (NULL);
 
