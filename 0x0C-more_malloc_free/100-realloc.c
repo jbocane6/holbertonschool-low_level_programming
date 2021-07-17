f2702c34b8d2f02b4b9cfc26d1f2c9f89660045b
root <root@DESKTOP-DVQ378U.localdomain>
2021-07-17 14:59:28 -0500

    file 4 v.3

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _realloc - reallocates a memory block.
@@ -14,8 +15,8 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *str;
-	int i;
+	char *str, *rtp = ptr;
+	unsigned int i, size = strlen(ptr);
 
 	if (ptr == NULL)
 		return (malloc(new_size));
@@ -34,12 +35,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	if (new_size > old_size)
-		for (i = 0; ptr[i] != '\0'; i++)
-			str[i] = ptr[i];
+	{
+		for (i = 0; i < size; i++)
+			str[i] = rtp[i];
+	}
 
 	if (new_size < old_size)
+	{
 		for (i = 0; i < new_size; i++)
-			str[i] = ptr[i];
+			str[i] = rtp[i];
+	}
 
 		return (str);
 }
