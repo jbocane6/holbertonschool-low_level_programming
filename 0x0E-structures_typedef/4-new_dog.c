23705d36b56d15ff576f498b4b54a8ba62adf7cd
root <root@DESKTOP-DVQ378U.localdomain>
2021-07-19 14:44:47 -0500

    file 4 v.2 adding cases where receives strings

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - initialize a variable of type dog_t
@@ -12,11 +13,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *inner_dog;
+	int i;
+	int size_name = strlen(name), size_owner = strlen(owner);
+	char *inner_name, *inner_owner;
 
 	inner_dog = malloc(sizeof(dog_t));
 
 	if (inner_dog == NULL)
 		return (NULL);
+
+	inner_name = malloc(sizeof(char) * size_name + 1);
+	if (inner_name == NULL)
+	{
+		free(inner_name);
+		free(inner_dog);
+	}
+	inner_owner = malloc(sizeof(char) * size_owner + 1);
+	if (inner_owner == NULL)
+	{
+		free(inner_owner);
+		free(inner_name);
+		free(inner_dog);
+	}
+
+	for (i = 0; i < size_name; i++)
+		inner_name[i] = name[i];
+
+	for (i = 0; i < size_owner; i++)
+		inner_owner[i] = owner[i];
 
 	inner_dog->name = name;
 	inner_dog->age = age;
