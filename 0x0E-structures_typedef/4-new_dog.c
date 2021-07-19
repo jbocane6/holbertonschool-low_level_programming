9092603a17c37d924919df59dcc259f5ed41d087
root <root@DESKTOP-DVQ378U.localdomain>
2021-07-19 15:22:44 -0500

    file 4 v.6 changing strlen by function _strlen

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,23 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _strlen - calculate the length of an array
+ * @str: array to calculate length
+ * Return: length
+*/
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i - 1);
+
+}
 
 /**
  * new_dog - initialize a variable of type dog_t
@@ -13,14 +30,14 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *inner_dog;
-	int i;
-	int size_name = strlen(name), size_owner = strlen(owner);
+	int i, size_name, size_owner;
 	char *inner_name, *inner_owner;
 
 	inner_dog = malloc(sizeof(dog_t));
-
 	if (inner_dog == NULL)
 		return (NULL);
+
+	size_name = _strlen(name);
 
 	inner_name = malloc(sizeof(char) * size_name + 1);
 	if (inner_name == NULL)
@@ -29,6 +46,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(inner_dog);
 		return (NULL);
 	}
+
+	size_owner = _strlen(owner);
+
 	inner_owner = malloc(sizeof(char) * size_owner + 1);
 	if (inner_owner == NULL)
 	{
@@ -38,15 +58,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	for (i = 0; i <= size_name; i++)
+	for (i = 0; i < size_name; i++)
 		inner_name[i] = name[i];
-
-	for (i = 0; i <= size_owner; i++)
+	for (i = 0; i < size_owner; i++)
 		inner_owner[i] = owner[i];
 
 	inner_dog->name = name;
 	inner_dog->age = age;
 	inner_dog->owner = owner;
-	
+
 	return (inner_dog);
 }
