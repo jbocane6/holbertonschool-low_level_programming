d55b2cfe2479acafc14291bdbaccbe6b4b854c5f
root <root@DESKTOP-DVQ378U.localdomain>
2021-07-19 12:38:39 -0500

    first commit and file 0 v.1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: struct type dog
+ * @name: pointer to an char string
+ * @age: float value numbre
+ * @owner: pointer to an char string
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    
+} 
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+## holbertonschool-low_level_programming
+
+### 0x0E. Repository with scripts of evidences:
+
+- 0x0E-structures_typedef
