8f8630808a95fa886415dfa494f953e7593dda16
root <root@DESKTOP-DVQ378U.localdomain>
2021-07-19 15:09:17 -0500

    file 4 v.5 reducing size of name and owner in 1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,26 +25,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 	inner_name = malloc(sizeof(char) * size_name + 1);
 	if (inner_name == NULL)
 	{
+		free(inner_name);
 		free(inner_dog);
 		return (NULL);
 	}
 	inner_owner = malloc(sizeof(char) * size_owner + 1);
 	if (inner_owner == NULL)
 	{
+		free(inner_owner);
 		free(inner_name);
 		free(inner_dog);
 		return (NULL);
 	}
 
-	for (i = 0; i < size_name; i++)
+	for (i = 0; i < size_name - 1; i++)
 		inner_name[i] = name[i];
 
-	for (i = 0; i < size_owner; i++)
+	for (i = 0; i < size_owner - 1; i++)
 		inner_owner[i] = owner[i];
 
 	inner_dog->name = name;
 	inner_dog->age = age;
 	inner_dog->owner = owner;
-
+	
 	return (inner_dog);
 }
