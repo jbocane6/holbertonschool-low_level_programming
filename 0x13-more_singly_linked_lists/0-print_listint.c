8e8e482ebca668797ef45691f4deb1e6750a79c0
Juan Bocanegra <jbocane6@gmail.com>
2021-08-03 13:43:38 -0500

    file 0 v.1 update lists.h

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: Pointer to a linked list structure.
+ * Return: The number of nodes.
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+	char c;
+
+	for (i = 0; h != NULL; i++)
+	{
+		c = h->n + '0';
+		write(1, &c, 1);
+		h = h->next;
+		c = '\n';
+		write(1, &c, 1);
+	}
+	return (i);
+}
