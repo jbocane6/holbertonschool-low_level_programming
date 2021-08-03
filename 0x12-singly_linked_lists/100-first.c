016cdffedae94af34ff994ddcfed7ef07d1b1af8
Juan Bocanegra <jbocane6@gmail.com>
2021-08-03 12:01:09 -0500

    file 100 v.1 update lists.h

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_after - before the main function is executed.
+ **/
+
+void print_after(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
