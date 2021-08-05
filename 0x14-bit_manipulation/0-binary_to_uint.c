9a5c754645488b932ee6e8a4019c97b50683119f
Juan Bocanegra <jbocane6@gmail.com>
2021-08-05 17:49:08 -0500

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,35 @@
 #include "main.h"
 
 /**
- * funcion - 
- * @variable: 
- * @variable: 
- * Return: 
+ * binary_to_uint - convert to decimal
+ * @d: string with binary
+ * Return: decimal or 0
 */
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int size = strlen(b);
-    unsigned int counter, exp = 1, sum = 0, i;
+	unsigned int size = strlen(b);
+	unsigned int counter, exp = 1, sum = 0, i;
 
-    if (b == NULL)
-        return (0);
+	if (b == NULL)
+		return (0);
 
-    for (counter = 0; counter < size; counter++)
-    {
-        if (b[counter] != '0' && b[counter] != '1' )
-            return (0);
+	for (counter = 0; counter < size; counter++)
+	{
+		if (b[counter] != '0' && b[counter] != '1')
+			return (0);
 
-        exp = 1;
-        for (i = 0; i < counter; i++)
-        {
-            if (counter == 0)
-            {
-                exp = 1;
-                break;
-            }
-            exp = 2 * exp;
-        }            
-        sum = sum + (exp * (int)(b[size - 1 -  counter] - '0'));
-    }
-    return (sum);
+		exp = 1;
+		for (i = 0; i < counter; i++)
+		{
+			if (counter == 0)
+			{
+				exp = 1;
+				break;
+			}
+			exp = 2 * exp;
+		}
+		sum = sum + (exp * (int)(b[size - 1 -  counter] - '0'));
+	}
+	return (sum);
 }
