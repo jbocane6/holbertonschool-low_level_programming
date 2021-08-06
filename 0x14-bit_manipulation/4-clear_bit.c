3000c597eaee0c7838ae3fb2edd47d7980f52fad
Juan Bocanegra <jbocane6@gmail.com>
2021-08-05 21:32:56 -0500

    File 4 v.2 Betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - Sets the value of a bit to 0 at a given index.
+ * clear_bit - Sets the value of a bit to 0 at a given index.
  * @n: Pointer to a number that has the value to modify
  * @index: Is the index, starting from 0 of the bit to set.
  * Return: If it worked, or -1 if an error occurred
