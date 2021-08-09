24592e84cbfe9318713b7d9991f46962c840a8f2
Juan Bocanegra <jbocane6@gmail.com>
2021-08-09 11:51:06 -0500

    file 1 v.1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: Name of the file to create.
+ * @text_content: NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure .
+ * If filename is NULL return -1
+ * If text_content is NULL create an empty file
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+
+	if (!filename || !text_content)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (fd);
+
+	w = write(fd, text_content, strlen(text_content));
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	return (1);
+}
