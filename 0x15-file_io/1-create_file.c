77f03578280471e2914db91d5515a0e60e563fa7
Juan Bocanegra <jbocane6@gmail.com>
2021-08-09 12:30:41 -0500

    file 1 v.2 adding closer file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		return (fd);
+		return (-1);
 
 	w = write(fd, text_content, strlen(text_content));
 	if (w == -1)
@@ -26,5 +26,6 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
+	close(fd);
 	return (1);
 }
