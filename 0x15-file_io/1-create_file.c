e57c5828470272eccf16b12a9027cd18e8708e55
Juan Bocanegra <jbocane6@gmail.com>
2021-08-09 12:33:09 -0500

    file 1 v.3 if text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,18 +13,21 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, w;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		return (-1);
+		return (fd);
 
-	w = write(fd, text_content, strlen(text_content));
-	if (w == -1)
+	if (text_content)
 	{
-		close(fd);
-		return (-1);
+		w = write(fd, text_content, strlen(text_content));
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
