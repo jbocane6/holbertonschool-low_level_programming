a9f861e52fd0a6fc65748927dea95600a6b11b8c
Juan Bocanegra <jbocane6@gmail.com>
2021-08-11 23:43:30 -0500

    rot13 correction

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
-
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * rot13 - Main function
  * Description: function that encodes a string using rot13.
@@ -10,26 +12,35 @@
 char *rot13(char *str)
 {
 	char *rts;
+	char *cadena1, *cadena1a, *cadena2, *cadena2a;
 	int cont, cont2, len = strlen(str);
-	
-	rts = malloc(sizeof(char) * len);
-	if (rts == NULL)
-		return;
-	
-	char cadena1[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz !_-,;?"};
-	char cadena2[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz !_-,;?"};
 
-	for (cont = 0; str[cont]; cont++)
+	rts = malloc(sizeof(char) * len);
+	if (rts != NULL)
 	{
-		for (cont2 = 0; cadena1[cont2]; cont2++)
+		*cadena1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijk';
+		*cadena1a = 'lmnopqrstuvwxyz ()\".!_-,:;?0123456789';
+		*cadena2 = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwx';
+		*cadena2a = 'yzabcdefghijklm ()\".!_-,:;?0123456789';
+
+		for (cont = 0; str[cont]; cont++)
 		{
-			if (cadena1[cont2] == str[cont])
+			for (cont2 = 0; cadena1[cont2]; cont2++)
 			{
-				rts[cont] = cadena2[cont2];
-				break;
+				if (cadena1[cont2] == str[cont])
+				{
+					rts[cont] = cadena2[cont2];
+					break;
+				}
+				if (cadena1a[cont2] == str[cont])
+				{
+					rts[cont] = cadena2a[cont2];
+					break;
+				}
 			}
 		}
 	}
-	return (rts);
+	str = rts;
 	free(rts);
+	return (str);
 }
