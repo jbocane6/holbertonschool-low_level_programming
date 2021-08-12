93269e428631956e1a5cd246d0dbe1a3b003487f
Juan Bocanegra <jbocane6@gmail.com>
2021-08-11 23:54:11 -0500

    rot13 remove special chars

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
 /**
  * rot13 - Main function
  * Description: function that encodes a string using rot13.
@@ -12,32 +11,29 @@
 char *rot13(char *str)
 {
 	char *rts;
-	char *cadena1, *cadena1a, *cadena2, *cadena2a;
-	int cont, cont2, len = strlen(str);
+	int cont, cont2, len = strlen(str), token;
 
 	rts = malloc(sizeof(char) * len);
 	if (rts != NULL)
 	{
-		*cadena1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijk';
-		*cadena1a = 'lmnopqrstuvwxyz ()\".!_-,:;?0123456789';
-		*cadena2 = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwx';
-		*cadena2a = 'yzabcdefghijklm ()\".!_-,:;?0123456789';
+		char cadena1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		char cadena2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 		for (cont = 0; str[cont]; cont++)
 		{
+			token = 0;
+
 			for (cont2 = 0; cadena1[cont2]; cont2++)
 			{
 				if (cadena1[cont2] == str[cont])
 				{
 					rts[cont] = cadena2[cont2];
-					break;
-				}
-				if (cadena1a[cont2] == str[cont])
-				{
-					rts[cont] = cadena2a[cont2];
+					token = 1;
 					break;
 				}
 			}
+			if (token == 0)
+				rts[cont] = str[cont];
 		}
 	}
 	str = rts;
