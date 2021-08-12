2f85de7eccf3c85c6015ece770e90320f73aee9d
Juan Bocanegra <jbocane6@gmail.com>
2021-08-11 23:08:17 -0500

    file 101 v.1

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 80;
+		sum += (pass[i]);
+		putchar(pass[i] + '0');
+		if ((2772 - sum) < 80)
+		{
+			n = 2772 - sum;
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
