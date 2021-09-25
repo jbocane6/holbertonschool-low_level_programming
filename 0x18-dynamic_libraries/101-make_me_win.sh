365ff4df208abac5ee429131bb4df5b9176da747
Juan Bocanegra <jbocane6@gmail.com>
2021-09-24 19:00:31 -0500

    solved

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/jbocane6/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries\make_me_win.so
+wget -P /tmp https://github.com/jbocane6/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/make_me_win.so
 export LD_PRELOAD=/tmp/make_me_win.so
